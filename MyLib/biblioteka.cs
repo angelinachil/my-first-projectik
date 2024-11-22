6a22ba5dbaf8fc914ad55ff577b13b8d7456458f
gelyachill <angelinacuguevskaa08@gmail.com>
2024-11-22 16:27:24 +0900

    Good solution

---
+++ MyLib/biblioteka.cs
@@ -0,0 +1,52 @@
+﻿namespace MyLib
+{
+    public class biblioteka
+    {
+        public void PrintMultiplicationTable(int number)
+        {
+            Console.WriteLine($"Таблица умножения для {number}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} x {i} = {number * i}");
+            }
+        }
+        //Метод, проверяющий определенное число на четность(не возвращает значения)
+        public void CheckEvenOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} - четное число.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} - нечетное число.");
+            }
+        }
+        /*Метод, возвращающий значение, вычисляющий площадь круга*/
+        public double CalculateCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+        /*Метод, вычисляющий факториал*/
+        public long CalculateFactorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Факториал определен только для неотрицательных чисел.");
+            }
+            if (n == 0) return 1;
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        /*Метод, проверяет год на високосность*/
+        public string IsLeapYear(int year)
+        {
+            bool isLeap = DateTime.IsLeapYear(year);
+            return $"{year} {(isLeap ? "високосный" : "не високосный")} год.";
+        }
+    }
+}
---
+++ firstDll/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyLib;
+
+biblioteka math = new biblioteka();
+math.PrintMultiplicationTable(7);
+math.CheckEvenOdd(15);
+double area = math.CalculateCircleArea(5);
+Console.WriteLine($"Площадь круга: {area}");
+try
+{
+    long factorial = math.CalculateFactorial(6);
+    Console.WriteLine($"Факториал 6: {factorial}");
+}
+catch (ArgumentException ex)//в случае ввода некорректных данных, программа вернет исключение
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
+string leapYearMessage = math.IsLeapYear(2024);
+Console.WriteLine(leapYearMessage);
+
+
+
+
+
