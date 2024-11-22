a5f4a5c2c5e1ab3c4238593e21bd0c7abeaec03b
gelyachill <angelinacuguevskaa08@gmail.com>
2024-11-22 16:44:18 +0900

    Perfect papka

---
+++ exeption/exeption/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class ExceptionHandlingExample
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            string input = "abc";
+            int number = int.Parse(input); // Попытка преобразовать строку в число
+            Console.WriteLine($"Число: {number}");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Ошибка преобразования: {ex.Message}");
+        }
+    }
+}
+
